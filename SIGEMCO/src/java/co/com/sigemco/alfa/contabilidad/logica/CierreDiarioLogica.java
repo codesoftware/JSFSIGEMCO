d932fd8fc03c2c33e8be39d40a0a7be403c62ab4
jnsierra <jnsierrac@gmail.com>
2016-01-09 19:22:22 -0500

    09/01/2016 003
    cambio para la configuracion del pool de conexiones

--- SIGEMCO/src/java/co/com/sigemco/alfa/contabilidad/logica/CierreDiarioLogica.java
+++ SIGEMCO/src/java/co/com/sigemco/alfa/contabilidad/logica/CierreDiarioLogica.java
@@ -18,7 +18,6 @@ import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 import java.util.ResourceBundle;
 import java.util.logging.Level;
